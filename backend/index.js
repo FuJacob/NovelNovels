8fbf3374ba53be3cbc2c382a926a35a6bfecf32f
Jason <j.li54@share.epsb.ca>
2024-09-14 17:44:14 -0400

    Remove open ai

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 // const cors = require('cors');
 // app.use(cors()); // Allow cross-origin requests
 app.use(express.json()); // Parse JSON bodies
-const OpenAI = require("openai");
 const axios = require('axios');
 const path = require('path');
 const port = 5000;
@@ -21,32 +20,9 @@ async function ImageGenerator(prompt) {
   }
 }
 
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-});
-
-// Generate image using open ai
-/* async function ImageGenerator(prompt) {
-  
-  const image = await openai.images.generate({
-    model: "dall-e-3",
-    prompt: "a white siamese cat",
-    n: 1,
-    response_format: "b64_json"
-  }); // this was with open ai
-
-  url = String.format("https://pollinations.ai/p/{0}", prompt);
-
-  console.log(image.data);
-} */
-
 ImageGenerator("Please help me");
 
 app.get('/generate_image', (req, res) => {
-  
-  // const {body} = req
-  // const {prompt} = body
   return res.json({image: ImageGenerator("Fun")})  // return a binary string to later be parsed in a binary string
 })
 
