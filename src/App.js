298de3938c201e34626a4fe2484d6ee13d6d3392
taymoorfarooq <taymoor.farooq@mail.utoronto.ca>
2024-09-14 13:10:20 -0400

    App css + js navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="navbar">
+          <div className='navbar-left'>
+            <h3>storyAI</h3>
+          </div>
+          <div className='navbar-right'>
+            <h3>Settings</h3>
+          </div>
+        </div>
+        
       </header>
     </div>
   );
