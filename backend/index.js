575dbe7d233574c6d3b99cefab0249c3446045d9
Jason <j.li54@share.epsb.ca>
2024-09-14 15:21:31 -0400

    Delete image gen.js

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,25 @@ const express = require('express')
 const app = express()
 require('dotenv').config();
 
+import OpenAI from "openai"; 
 
-// respond with "hello world" when a GET request is made to the homepage
+if (process.env.OPENAI_API_KEY) {
+  // The environment variable is set and not empty
+  const openai = new OpenAI({
+      dangerouslyAllowBrowser: true,
+      apiKey: process.env.OPENAI_API_KEY
+  });
+  console.log(process.env.OPENAI_API_KEY);
+} else {
+  console.error('OPENAI_API_KEY is not set.');
+}
+
+function ImageGenerator(prompt) {
+  
+}
+
+ImageGenerator("Please help me");
+
 app.get('/generate_image', (req, res) => {
   const key =  process.env.OPENAI_API_KEY
   console.log(key);
