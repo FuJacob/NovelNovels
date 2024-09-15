ece9c131836cf5ea1c1125f737cc4f7eaca12531
Jason <j.li54@share.epsb.ca>
2024-09-15 05:05:28 -0400

    Change path separator to /

--- backend/books.js
+++ backend/books.js
@@ -10,14 +10,14 @@ function Book(title, author, year, path, cover) {
     }
 }
 
-const OLD_MAN_OF_THE_SEA = new Book("The Old Man of the Sea", "William Wymark Jacobs", 1900, "..\\assets\\books\\old-man-and-the-sea.txt")
-const GOLDEN_BIRD = new Book("The Golden Bird", "Brothers Grimm", 1812, "..\\assets\\books\\golden-bird.txt")
-const ALICE_IN_WONDERLAND = new Book("Alice in Wonderland", "Lewis Caroll", 1865, "..\\assets\\books\\alice-in-wonderland.txt")
-const A_MODEST_PROPOSAL = new Book("A Modest Proposal", "Jonathan Swift", 1729, "..\\assets\\books\\a-modest-proposal.txt")
-const GREAT_GATSBY =  new Book("The Great Gatsby", "Francis Scott Fitzgerald", 1925, "..\\assets\\books\\great-gatsby.txt")
-const ETHAN_FROME = new Book("Ethan Frome", "Edith Wharton", 1911, "..\\assets\\books\\ethan-frome.txt")
-const JEKYLL_AND_HYDE = new Book("Dr. Jekyll and Hyde", "Robert Louis Stevenson", 1886, "..\\assets\\books\\jekyll-and-hyde.txt")
-const THREE_LITTLE_PIGS = new Book("The Three Little Pigs", "Folklore", 1890, "..\\assets\\books\\three-little-pigs.txt")
+const OLD_MAN_OF_THE_SEA = new Book("The Old Man of the Sea", "William Wymark Jacobs", 1900, "../assets/books/old-man-and-the-sea.txt")
+const GOLDEN_BIRD = new Book("The Golden Bird", "Brothers Grimm", 1812, "../assets/books/golden-bird.txt")
+const ALICE_IN_WONDERLAND = new Book("Alice in Wonderland", "Lewis Caroll", 1865, "../assets/books/alice-in-wonderland.txt")
+const A_MODEST_PROPOSAL = new Book("A Modest Proposal", "Jonathan Swift", 1729, "../assets/books/a-modest-proposal.txt")
+const GREAT_GATSBY =  new Book("The Great Gatsby", "Francis Scott Fitzgerald", 1925, "../assets/books/great-gatsby.txt")
+const ETHAN_FROME = new Book("Ethan Frome", "Edith Wharton", 1911, "../assets/books/ethan-frome.txt")
+const JEKYLL_AND_HYDE = new Book("Dr. Jekyll and Hyde", "Robert Louis Stevenson", 1886, "../assets/books/jekyll-and-hyde.txt")
+const THREE_LITTLE_PIGS = new Book("The Three Little Pigs", "Folklore", 1890, "../assets/books/three-little-pigs.txt")
 
 
 module.exports = {
