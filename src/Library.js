a3766a11c5803b6203bd6955f61d1f1b06c85997
taymoorfarooq <taymoor.farooq@mail.utoronto.ca>
2024-09-15 03:53:42 -0400

    add covers update library

--- src/Library.js
+++ src/Library.js
@@ -28,9 +28,15 @@ function Library() {
           <p>Welcome to your personal library. Here you can view and manage your books.</p>
           <div className="book-list">
             {/* Add a list of books or placeholder content here */}
-            <div className="book-item">Book 1</div>
+            <Link to="/0" className="book-item"><div className="book-item">Book 1</div></Link>
+            
             <div className="book-item">Book 2</div>
             <div className="book-item">Book 3</div>
+            <div className="book-item">Book 4</div>
+            <div className="book-item">Book 5</div>
+            <div className="book-item">Book 6</div>
+            <div className="book-item">Book 7</div>
+            <div className="book-item">Book 8</div>
           </div>
           <Link to="/" className="back-button">Back to Home</Link>
         </div>
